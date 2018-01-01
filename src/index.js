c44616185bf2b82707d1f00837b0f5812f87e123
Jared Pleva <jpleva91@gmail.com>
2017-12-31 21:26:42 -0700

    changes index.js to component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD33OcNFOoxDCb8GBk97BC-D1EKuWSfeKg';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />,
-  document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('.container'));
